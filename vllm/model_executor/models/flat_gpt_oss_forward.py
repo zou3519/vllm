e6b8e17eafbf9c2f5f91e9dab03869d120bf5e1f
Richard Zou <zou3519@gmail.com>
2026-05-07 12:58:10 -0700

    Drop flat MoE output no-op slice

--- vllm/model_executor/models/flat_gpt_oss_forward.py
+++ vllm/model_executor/models/flat_gpt_oss_forward.py
@@ -203,7 +203,7 @@ def transformer_layer(
         moe_local_num_experts,
         moe_routing_method_type,
         moe_tune_max_num_tokens,
-        hidden_size,
+        _hidden_size,
     ) = layer_params
 
     # TransformerBlock.input_layernorm
@@ -347,7 +347,6 @@ def transformer_layer(
         moe_routing_method_type,
         moe_tune_max_num_tokens,
     )
-    output = output[:, :hidden_size]
 
     return output, residual
 
