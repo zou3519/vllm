4982c6dbe0b30ec866c7c78ebb304febce961781
Richard Zou <zou3519@gmail.com>
2026-05-08 08:42:34 -0700

    Tune fused sparse qk prep warps

--- vllm/model_executor/models/flat_deepseek_v32_forward.py
+++ vllm/model_executor/models/flat_deepseek_v32_forward.py
@@ -546,6 +546,7 @@ def transformer_layer(
                 EPS=indexer.k_norm.eps,
                 FACTOR=indexer.softmax_scale * indexer.n_head**-0.5,
                 BLOCK_N=128,
+                num_warps=8,
             )
             index_weights = scaled_index_weights
             topk_indices_buffer = indexer.topk_indices_buffer
