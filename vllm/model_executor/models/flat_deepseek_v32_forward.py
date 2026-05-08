5db3a9510d301c5e9076a41eb8a6ab5c999f528c
Richard Zou <zou3519@gmail.com>
2026-05-07 21:14:28 -0700

    Use narrower MLA concat Triton tile

--- vllm/model_executor/models/flat_deepseek_v32_forward.py
+++ vllm/model_executor/models/flat_deepseek_v32_forward.py
@@ -622,7 +622,7 @@ def transformer_layer(
                 mqa_ql_nope.shape[2] + mqa_q_pe.shape[2],
             )
         )
-        _mla_decode_q_concat_kernel[((decode_q0.numel() + 255) // 256,)](
+        _mla_decode_q_concat_kernel[((decode_q0.numel() + 127) // 128,)](
             mqa_ql_nope,
             mqa_q_pe,
             decode_q0,
@@ -637,7 +637,7 @@ def transformer_layer(
             mqa_q_pe.stride(0),
             mqa_q_pe.stride(1),
             mqa_q_pe.stride(2),
-            BLOCK_N=256,
+            BLOCK_N=128,
         )
         decode_q_flat = decode_q0.reshape(decode_q0.shape[0], -1)
         mqa_q, _ = ops.scaled_fp8_quant(
