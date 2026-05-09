8a65c76dd00741c96b8c5611bf587de77ff82cf3
Richard Zou <zou3519@gmail.com>
2026-05-08 21:37:28 -0700

    Revert "Tune raw MLA prep CTA to 768 threads"
    This reverts commit 8614adc0a407055aeca845040ac89157e3dfff9f.

--- csrc/mla_prep.cu
+++ csrc/mla_prep.cu
@@ -6,7 +6,7 @@
 
 namespace vllm {
 
-constexpr int kMlaPrepThreads = 768;
+constexpr int kMlaPrepThreads = 1024;
 
 __device__ __forceinline__ float load_bf16(const __nv_bfloat16* ptr,
                                            int64_t offset) {
