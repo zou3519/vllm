e951c445fdfd6e2aac38d7d99a91c5501778515d
Richard Zou <zou3519@gmail.com>
2026-05-10 10:34:00 -0700

    Revert "Tune short indexer K cache kernel to two warps"
    This reverts commit 3c12aac5d81671cebc3421f916f583d3102f84bb.

--- vllm/model_executor/models/flat_deepseek_v32_forward.py
+++ vllm/model_executor/models/flat_deepseek_v32_forward.py
@@ -601,7 +601,6 @@ def transformer_layer(
                     indexer.k_cache.kv_cache.shape[2],
                     EPS=indexer.k_norm.eps,
                     BLOCK_N=128,
-                    num_warps=2,
                 )
             else:
                 _index_qk_rope_quant_cache_kernel[
