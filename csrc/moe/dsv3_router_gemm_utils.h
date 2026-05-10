86152fe26423e7cace881115bf8c7262ec0938e2
Richard Zou <zou3519@gmail.com>
2026-05-10 12:58:16 -0700

    Enable PDL for router GEMM by default

--- csrc/moe/dsv3_router_gemm_utils.h
+++ csrc/moe/dsv3_router_gemm_utils.h
@@ -36,7 +36,7 @@ inline bool getEnvEnablePDL() {
   std::call_once(flag, [&]() {
     if (getSMVersion() >= 90) {
       const char* env = std::getenv("TRTLLM_ENABLE_PDL");
-      enablePDL = env && env[0] == '1' && env[1] == '\0';
+      enablePDL = env ? env[0] == '1' && env[1] == '\0' : true;
     }
   });
   return enablePDL;
